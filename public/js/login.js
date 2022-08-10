bba43e7b3c038f90d4cb88ed68bb39b99adab55c
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-08-10 12:57:27 -0600

    create header and add body handlebar element in main.handlebar

---
+++ public/js/login.js
@@ -0,0 +1,15 @@
+
+// const loginFromHandeler = async(event) => {
+//   event.preventDefault();
+
+//   const email = document.querySelector('#email-login').value.trim();
+//   const password = document.querySelector('password-login').value.trim();
+
+//   if (email && password) {
+
+//     const response = await fetch 
+//   }
+
+
+
+// }
