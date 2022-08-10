13bc73d9cf319d0c42fcd31d3c8f56c74e7b3395
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-08-10 13:08:54 -0600

    create engine for handlebars in server.js

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(session(sess));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
 
 // deconstruct helpers from express-handlebar library and save it into variable hbs
 const hbs = exphbs.create({ helpers })
@@ -21,6 +19,14 @@ app.get('/', (req, res) => res.send ('hello world'))
 
 // // turn on routes
 // app.use(routes);
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars')
+
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // turn on connection to db and server
 // sequelize.sync({ force: false }).then(() => {
