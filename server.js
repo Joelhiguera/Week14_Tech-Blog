5731d0da477f26f88f3499937c918383a7eabafc
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-08-31 22:38:54 -0600

    add helpers sess exphbs variables in server.js

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 
-// const routes = require('./routes');
-// const sequelize = require('./config/connection');
+
+
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,23 +20,30 @@ const PORT = process.env.PORT || 3001;
 // deconstruct helpers from express-handlebar library and save it into variable hbs
 const hbs = exphbs.create({ helpers })
 
-app.get('/', (req, res) => res.send ('hello world'))
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-// // turn on routes
-// app.use(routes);
+
+
+
+app.use(session(sess));
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars')
 
 
-app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// turn on connection to db and server
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log('Now listening'));
-// });
+
 
 app.listen(PORT, () => {
   console.log(`Now listening on ${PORT}`)
